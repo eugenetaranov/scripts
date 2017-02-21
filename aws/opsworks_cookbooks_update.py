2a02edccec1aaf0ccd57430817306df20053aea6
Eugene Taranov <eugene@taranov.me>
2017-02-21 22:24:54 +0200

    Update opsworks_cookbooks_update.py

--- aws/opsworks_cookbooks_update.py
+++ aws/opsworks_cookbooks_update.py
@@ -2,15 +2,18 @@
 '''
 1. Runs berks package -b {dir}/Berksfile on all subdirectories with Berksfiles
 2. Uploads cookbook archive to s3 bucket
+3. Updates stack settings pointing to new cookbook archive
 3. Runs custom_cookbook_update
 4. Runs setup
 '''
+
 import os, boto3, subprocess
 from time import sleep
 
 
-COOKBOOKS_BUCKET = ''
-STACK_ID = ''
+COOKBOOKS_BUCKET = '427985443494-opsworks'
+STACK_ID = '79a167e6-3e71-43d0-89e7-0c9de1bdd2b7'
+RUN_SETUP = True
 
 
 def cookbook_build():
@@ -32,7 +35,15 @@ class Opsworks(object):
     def __init__(self):
         self.conn = boto3.client('opsworks')
 
-    def stack_update(self, stack_id):
+    def stack_update(self, stack_id, s3_bucket, artifact_name):
+        self.conn.update_stack(
+            StackId=stack_id,
+            CustomCookbooksSource={
+                'Type': 's3',
+                'Url': 'https://s3.amazonaws.com/{}/{}'.format(s3_bucket, artifact_name)
+                })
+
+    def stack_update_cookbooks(self, stack_id):
         res = self.conn.create_deployment(StackId=stack_id, Command={'Name': 'update_custom_cookbooks'})
 
         n = 0
@@ -53,7 +64,7 @@ class Opsworks(object):
                 return
             sleep(10)
             n = n + 1
-        
+
         raise Exception('Timeout')
 
 
@@ -61,9 +72,13 @@ def main():
     artifact_name = cookbook_build()
     artifact_s3_upload(artifact_name, COOKBOOKS_BUCKET)
     os.unlink(artifact_name)
+    
     ops_client = Opsworks()
-    ops_client.stack_update(STACK_ID)
-    ops_client.stack_setup(STACK_ID)
+    ops_client.stack_update(STACK_ID,COOKBOOKS_BUCKET, artifact_name)
+    ops_client.stack_update_cookbooks(STACK_ID)
+
+    if RUN_SETUP:
+        ops_client.stack_setup(STACK_ID)
 
 
 if __name__ == '__main__':
