4a62a355bcdc655a054ace3a573102a8f6017126
e <e@ip-192-168-1-4.ec2.internal>
2024-04-22 15:57:24 +0200

    dded ssm_search

---
+++ aws/ssm_search/README.md
@@ -0,0 +1,13 @@
+```bash
+usage: ssm_search.py [-h] [--cache CACHE] [--parameter PARAMETER] [--list] [--reset] [--search [SEARCH ...]]
+
+options:
+  -h, --help            show this help message and exit
+  --cache CACHE         Cache path
+  --parameter PARAMETER
+                        Parameter name
+  --list                List all parameters
+  --reset               Reset cache
+  --search [SEARCH ...]
+                        Search keywords
+```
---
+++ aws/ssm_search/ssm_search.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import boto3
+import botocore
+import sys
+from diskcache import Cache
+from argparse import ArgumentParser
+
+
+BOTO3_CONFIG = botocore.config.Config(retries=dict(max_attempts=30))
+
+
+def parseargs():
+    p = ArgumentParser()
+    p.add_argument("--cache", required=False, default="~/.cache/ssm_search", help="Cache path")
+    p.add_argument("--parameter", required=False, help="Parameter name")
+    p.add_argument("--list", action="store_true", help="List all parameters")
+    p.add_argument("--reset", action="store_true", help="Reset cache")
+    p.add_argument("--search", nargs="*", help="Search keywords")
+    return vars(p.parse_args())
+
+
+class SsmClient:
+    def __init__(self, config: botocore.config.Config):
+        self.client = boto3.client("ssm", config=config)
+
+    def list_parameters(self, beginswith: str = ""):
+        paginator = self.client.get_paginator("describe_parameters")
+
+        if beginswith:
+            pages = paginator.paginate(
+                ParameterFilters=[
+                    {
+                        "Key": "Path",
+                        "Option": "Recursive",
+                        "Values": [
+                            beginswith,
+                        ],
+                    },
+                ],
+                PaginationConfig={
+                    "PageSize": 50,
+                },
+            )
+        else:
+            pages = paginator.paginate(
+                PaginationConfig={
+                    "PageSize": 50,
+                }
+            )
+
+        for p in pages:
+            yield p
+
+    def get_parameter(self, name: str) -> str:
+        return self.client.get_parameter(Name=name, WithDecryption=True)["Parameter"]["Value"]
+
+
+def main():
+    args = parseargs()
+    ssm = SsmClient(config=BOTO3_CONFIG)
+
+    cache = Cache(args["cache"])
+
+    if args["reset"]:
+        cache.clear()
+
+    if "all_parameters" not in cache:
+        all_parameters = []
+        for page in ssm.list_parameters(beginswith="/"):
+            for parameter in page["Parameters"]:
+                all_parameters.append(parameter["Name"])
+        cache["all_parameters"] = all_parameters
+
+    try:
+        # just list all parameters
+        if args["list"]:
+            for parameter in cache["all_parameters"]:
+                print(parameter)
+
+        # filter all matching parameters
+        if args["search"]:
+            for parameter_name in cache["all_parameters"]:
+                if all(f in parameter_name for f in args["search"]):
+                    print(parameter_name, ssm.get_parameter(name=parameter_name))
+
+    except KeyboardInterrupt:
+        exit(1)
+
+    # get just parameter by full name
+    if args["parameter"]:
+        parameter_value = ssm.client.get_parameter(Name=args["parameter"], WithDecryption=True)[
+            "Parameter"
+        ]["Value"]
+        print(parameter_value)
+
+
+if __name__ == "__main__":
+    main()
